816778fdda6c60eb9fab08f15f232af9ba88cdfe
Vinayak Mishra <vinayakmishract.2022@gmail.com>
2026-07-06 13:50:22 +0530

    Update Majority_Element.py

--- solutions/day-4/Majority_Element.py
+++ solutions/day-4/Majority_Element.py
@@ -1,19 +1,12 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-            HashMap<Integer,Integer> map=new HashMap<>();
-            for(int i=0;i<nums.length;i++){
-                if(map.containsKey(nums[i]))
-                map.put(nums[i],map.get(nums[i])+1);
-                else
-                map.put(nums[i],1);
-            }
-            int ans=0;
-            for(int key:map.keySet()){
-                if(map.get(key)>nums.length/2){
-                    ans=key;
-                    break;
-                }
-            }
-            return ans;
-    }
-}
+class Solution(object):
+    def majorityElement(self, nums):
+        n=len(nums)
+        nu=list(set(nums))
+        for i in nu:
+            c=0
+            for j in nums:
+                if i==j:
+                    c+=1
+            if c>n/2:
+                return i
+        return ""
