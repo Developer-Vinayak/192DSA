720b43b0e80158dacff15e35320a1889a8496229
Vinayak Mishra <vinayakmishract.2022@gmail.com>
2026-08-06 05:41:03 +0530

    Create Word_Break.py

---
+++ solutions/day-35/Word_Break.py
@@ -0,0 +1,15 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        word_set = set(wordDict)
+        max_len = max(map(len, wordDict), default=0)
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True  
+
+        for i in range(1, n + 1):
+            for j in range(max(0, i - max_len), i):
+                if dp[j] and s[j:i] in word_set:
+                    dp[i] = True
+                    break
+
+        return dp[n]
