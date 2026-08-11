d8f7837dc98df3ffd3785d7fffa9e452bb0886d6
Vinayak Mishra <vinayakmishract.2022@gmail.com>
2026-08-12 04:57:03 +0530

    Create Decode_String.py

---
+++ solutions/day-40/Decode_String.py
@@ -0,0 +1,20 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []      
+        current = ""     
+        num = 0            
+
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == '[':
+                stack.append((current, num))
+                current = ""
+                num = 0
+            elif ch == ']':
+                prev_string, k = stack.pop()
+                current = prev_string + current * k
+            else:
+                current += ch
+
+        return current
