df1eacfbc2e4a766aef916b50720b11d31eaeee5
Vinayak Mishra <vinayakmishract.2022@gmail.com>
2026-08-16 23:14:30 +0530

    Add Linked_List_Cycle_II solution
    Implement cycle detection in a linked list using Floyd's Tortoise and Hare algorithm.

---
+++ solutions/day-44/Linked_List_Cycle_II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                break
+        else:
+            return None  
+        ptr = head
+        while ptr != slow:
+            ptr = ptr.next
+            slow = slow.next
+        
+        return ptr
