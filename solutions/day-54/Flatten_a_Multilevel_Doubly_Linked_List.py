3cfcd80cfe007c9888d4229ced5d5294793e99fc
Vinayak Mishra <vinayakmishract.2022@gmail.com>
2026-08-26 19:01:03 +0530

    Create Flatten_a_Multilevel_Doubly_Linked_List.py

---
+++ solutions/day-54/Flatten_a_Multilevel_Doubly_Linked_List.py
@@ -0,0 +1,23 @@
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return head
+
+        curr = head
+        stack = []
+
+        while curr:
+            if curr.child:
+                if curr.next:
+                    stack.append(curr.next)
+                curr.next = curr.child
+                curr.child.prev = curr
+                curr.child = None
+            if not curr.next and stack:
+                next_node = stack.pop()
+                curr.next = next_node
+                next_node.prev = curr
+
+            curr = curr.next
+
+        return head
