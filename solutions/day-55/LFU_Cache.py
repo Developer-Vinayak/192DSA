330cbc73b4f8a535dc62f038086ba980a92b67ca
Vinayak Mishra <vinayakmishract.2022@gmail.com>
2026-08-27 21:48:35 +0530

    Update LFU_Cache.py

--- solutions/day-55/LFU_Cache.py
+++ solutions/day-55/LFU_Cache.py
@@ -6,7 +6,6 @@ class LFUCache:
         self.key_to_freq = {}
         self.freq_to_keys = defaultdict(OrderedDict) 
         self.min_freq = 0
-
     def _touch(self, key: int):
         freq = self.key_to_freq[key]
         del self.freq_to_keys[freq][key]
@@ -17,29 +16,24 @@ class LFUCache:
         new_freq = freq + 1
         self.key_to_freq[key] = new_freq
         self.freq_to_keys[new_freq][key] = None
-
     def get(self, key: int) -> int:
         if key not in self.key_to_val:
             return -1
         self._touch(key)
         return self.key_to_val[key]
-
     def put(self, key: int, value: int) -> None:
         if self.capacity <= 0:
             return
-
         if key in self.key_to_val:
             self.key_to_val[key] = value
             self._touch(key)
             return
-
         if len(self.key_to_val) >= self.capacity:
             evict_key, _ = self.freq_to_keys[self.min_freq].popitem(last=False)
             if not self.freq_to_keys[self.min_freq]:
                 del self.freq_to_keys[self.min_freq]
             del self.key_to_val[evict_key]
             del self.key_to_freq[evict_key]
-
         self.key_to_val[key] = value
         self.key_to_freq[key] = 1
         self.freq_to_keys[1][key] = None
