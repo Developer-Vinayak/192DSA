a9f6a33b477df87089b0cf142567f9baf7b197b3
Vinayak Mishra <vinayakmishract.2022@gmail.com>
2026-08-31 06:15:42 +0530

    Create Binary_Tree_Postorder_Traversal.py

---
+++ solutions/day-58/Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,11 @@
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        def dfs(node: Optional[TreeNode]):
+            if not node:
+                return
+            dfs(node.left)   
+            dfs(node.right)  
+            result.append(node.val)  
+        dfs(root)
+        return result
