46b805f89029dd906d378fb783cb1a1860c0c531
Vinayak Mishra <vinayakmishract.2022@gmail.com>
2026-09-03 06:01:35 +0530

    Create Path_Sum_II.py

---
+++ solutions/day-61/Path_Sum_II.py
@@ -0,0 +1,20 @@
+class Solution:
+    def pathSum(self, root, targetSum):
+        result = []
+        path = []
+
+        def dfs(node, remaining):
+            if not node:
+                return
+            path.append(node.val)
+            remaining -= node.val
+
+            if not node.left and not node.right and remaining == 0:
+                result.append(list(path))
+            else:
+                dfs(node.left, remaining)
+                dfs(node.right, remaining)
+
+            path.pop() 
+        dfs(root, targetSum)
+        return result
