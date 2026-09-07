b64d66081e96e5feb685f287000451d90c90c10e
Vinayak Mishra <vinayakmishract.2022@gmail.com>
2026-09-07 14:25:02 +0530

    Create Count_Good_Nodes_in_Binary_Tree.py

---
+++ solutions/day-65/Count_Good_Nodes_in_Binary_Tree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node, max_so_far):
+            if not node:
+                return 0
+            
+            count = 1 if node.val >= max_so_far else 0
+            new_max = max(max_so_far, node.val)
+            
+            count += dfs(node.left, new_max)
+            count += dfs(node.right, new_max)
+            
+            return count
+        
+        return dfs(root, root.val)
