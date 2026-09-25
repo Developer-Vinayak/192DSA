2df61f3cef2608eb4fc15e329c84fadcb83cd1f5
Vinayak Mishra <vinayakmishract.2022@gmail.com>
2026-09-25 07:37:03 +0530

    Create Letter_Combinations_of_a_Phone_Number.py

---
+++ solutions/day-82/Letter_Combinations_of_a_Phone_Number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        letterMap = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"],
+        }
+        out = []
+        combo = []
+        def dfs(i):
+            if i >= len(digits):
+                string = "".join(combo)
+                out.append(string)
+                return
+            for letter in letterMap[digits[i]]:
+                combo.append(letter)
+                dfs(i + 1)
+                combo.pop()
+        dfs(0)
+        return out
